fb7e465bd50715c9a6913f581b53f3cb8c92caf1
Roderick Mabo <roderick.mabo@my.jcu.edu.au>
2023-10-01 15:09:03 +1000

    Add Random Number Generation Code

---
+++ Week_03/randoms.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 - Practical
+Code for randoms
+"""
+
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3,10,2))
+print(random.uniform(2.5, 5.5))  # line
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest? 6,20
+
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest? 5,9
+# Could line 2 have produced a 4? no starting with 5
+
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest? 2.51 5.4
+
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+# print(random.randint(1,100)) # this print a random number between 1 and 100 inclusive
